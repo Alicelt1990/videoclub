5ed567fbff9d6968505c5e1c18ea16e2e877d27e
Alicelt1990 <105047477+Alicelt1990@users.noreply.github.com>
2023-11-27 11:08:46 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,79 +1,33 @@
-<?php
-    include "views.php";
-    include "login.php";
-    include "film.php";
-    include "person.php";
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
 
-    if (isset($_REQUEST["do"])) {
-        $do = $_REQUEST["do"];
-    }
-    else {
-        $do = "formLogin";
-    }
+    <title>Videoclub</title>
+    <!-- CSS -->
+    <link href="styles.css" rel="stylesheet">
+  </head>
 
-    switch ($do) {
-    case "formLogin":
-        Login::mostrarFormLogin();
-        break;
-    case "tryLogin":
-        $result = Login::procesarFormLogin();
-        if ($result) {
-            View::display("main");
-        }
-        break;
-    case "showInsertFilm":
-        Film::showInsert();
-        break;
-    case "showInsertPerson":
-        Person::showInsert();
-        break;
-    case "insertarPeli":
-        Film::insertarPeli();
-        break;
-    case "insertPerson":
-        Person::insertPerson();
-        break;
-    case "showSearchFilm":
-        View::display("searchFilm");
-        break;
-    case "searchFilm":
-        Film::searchFilm();
-        break;
-    case "showSearchPerson":
-        View::display("searchPerson");
-        break;
-    case "searchPerson":
-        Person::searchPerson();
-        break;
-    case "showDeleteFilm":
-        View::display("deleteFilmForm");
-        break;
-    case "showDeletePerson":
-        View::display("deletePersonForm");
-        break;
-    case "deletePerson":
-        Person::deletePerson();
-        break;
-    case "deleteFilm":
-        Film::deleteFilm();
-        break;
-    case "showModifyFilm":
-        View::display("modifyFilmForm");
-        break;
-    case "modifyFilm":
-        Film::modifyFilm();
-        break;
-    case "showModifyPerson":
-        View::display("modifyPersonForm");
-        break;
-    case "modifyPerson":
-        Person::modifyPerson();
-        break;
-    case "main":
-        View::display("main");
-        break;
+  <body>
+    <div class="container">
+      <div class="header clearfix">
+        <h3 class="text-muted">Programación PHP</h3>
+      </div>
 
-    }
+      <div class="jumbotron">
+        <h1>Videoclub</h1>
+        <p class="lead">Videoclub</p>
+        <p><a class="btn btn-lg btn-success" href="busqueda.php" role="button">Buscar películas</a></p>
+		<a class="btn btn-lg btn-success" href="devolucion.php" role="button">Devolver películas</a>
+      </div>
 
-    ?>
-
+      <footer class="footer">
+        <p>&copy; 2023 DAW 2.</p>
+      </footer>
+    </div>
+  </body>
+</html>
